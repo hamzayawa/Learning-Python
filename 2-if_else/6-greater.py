48f7aa0511d9aee5d30ccecfd28265f5a30fb7d0
Hamza Abdullahi <coachbabaa@gmail.com>
2023-02-15 06:42:51 +0100

    Compare 2 numbers

---
+++ 2-if_else/6-greater.py
@@ -0,0 +1,9 @@
+#compare two numbers and find the largest
+
+a = input("enter a number: ")
+b = input("enter another number: ")
+
+if a > b:
+    print("First number is the largest")
+else:
+    print("Second number is the largest")
