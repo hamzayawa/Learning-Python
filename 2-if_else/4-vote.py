8a8774d3bd6b116bbbc13a95426bd77ec6afad34
Hamza Abdullahi <coachbabaa@gmail.com>
2023-02-15 06:39:13 +0100

    Check vote eligibility

---
+++ 2-if_else/4-vote.py
@@ -0,0 +1,7 @@
+# Check if a person is eligible to vote
+
+age = int (input("Enter your age?: "))
+if age>=18:
+	print("You are eligible to vote !!");
+else:
+	print("Sorry! you are not eligible to vote !!");
