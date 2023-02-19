c3b9b2de104981cb439b593aa71a6092fa7216f4
Hamza Abdullahi <coachbabaa@gmail.com>
2023-02-19 17:39:38 +0100

    Add tuple 2

---
+++ 4-tuple/2-tup.py
@@ -0,0 +1,9 @@
+# Creating a tuple
+tup = ('John', 'Doe', 30)
+
+# Unpacking a tuple
+first_name, last_name, age = tup
+
+print(first_name)   # Output: 'John'
+print(last_name)    # Output: 'Doe'
+print(age)          # Output: 30
