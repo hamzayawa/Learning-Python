74c48b05e10d7dbb25479ed84b8d6d30007d2fcf
Hamza Abdullahi <coachbabaa@gmail.com>
2023-02-28 05:16:34 +0100

    Set intersection, union, and difference

---
+++ 6-set/3-set_op.py
@@ -0,0 +1,17 @@
+# Create two sets
+set1 = {1, 2, 3, 4, 5}
+set2 = {3, 4, 5, 6, 7}
+
+# Compute the intersection of the two sets
+intersection = set1.intersection(set2)
+
+# Compute the union of the two sets
+union = set1.union(set2)
+
+# Compute the difference between the two sets
+difference = set1.difference(set2)
+
+# Print the results
+print(f"Intersection: {intersection}")
+print(f"Union: {union}")
+print(f"Difference: {difference}")
