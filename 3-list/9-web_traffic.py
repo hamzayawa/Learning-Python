f4d7f290b26f9ad2add0812e5cbfba77075ce3c9
Hamza Abdullahi <coachbabaa@gmail.com>
2023-02-28 04:43:12 +0100

    Analyzing web traffic data

---
+++ 3-list/9-web_traffic.py
@@ -0,0 +1,10 @@
+traffic_data = [150, 180, 300, 400, 600, 800, 900, 700, 500, 350, 200, 150]
+
+# Get the peak traffic hours (the top 3 hours)
+peak_traffic_hours = sorted(range(len(traffic_data)), key=lambda i: traffic_data[i], reverse=True)[:3]
+
+# Get the average traffic for the day (excluding the first and last hour)
+average_traffic = sum(traffic_data[1:-1]) / (len(traffic_data) - 2)
+
+print(peak_traffic_hours)
+print(average_traffic)
