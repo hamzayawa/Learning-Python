476ba4eb4020369738090b59c293147eef36a66b
Hamza Abdullahi <coachbabaa@gmail.com>
2023-02-28 05:15:06 +0100

    Remove duplicates from list

---
+++ 6-set/2-rm_duplicate.py
@@ -0,0 +1,11 @@
+# Create a list with duplicate elements
+my_list = [1, 2, 2, 3, 3, 4, 5, 5]
+
+# Convert the list to a set to remove duplicates
+my_set = set(my_list)
+
+# Convert the set back to a list
+new_list = list(my_set)
+
+# Print the new list
+print(new_list)
