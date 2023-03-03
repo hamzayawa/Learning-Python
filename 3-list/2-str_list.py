f81986fe7df9dc5931e41e54d43183b728c89ce6
Hamza Abdullahi <coachbabaa@gmail.com>
2023-03-03 06:47:20 +0100

    Udpate list

--- 3-list/2-str_list.py
+++ 3-list/2-str_list.py
@@ -1,11 +1,16 @@
 # A list of strings
-my_list = ['apple', 'banana', 'cherry', 'date']
+my_list = [
+        'apple', 
+        'banana', 
+        'cherry', 
+        'date',
+        ]
 
 print(my_list)
 
 #Accessing elements in a list
-print(my_list[0]) # Output: 'banana'
-print(my_list[-2]) # Output: 'cherry'
+print("Accessing first element of the list: ", my_list[0]) # Output: 'banana'
+print("Accessing Second to the last element of the list: ", my_list[-2]) # Output: 'cherry'
 
 # Modifying elements in a list 
 my_list[1] = 'orange'
@@ -14,6 +19,8 @@ print(my_list) # Output: ['apple', 'orange', 'cherry', 'date']
 # Slicing a list
 print(my_list[1:3]) # Output: ['orange', 'cherry']
 print(my_list[1:]) # Output: ['orange','cherry', 'date']
+print(my_list[::2])  # Output: ['apple', 'cherry']
+print(my_list[2::])  # Output: ['cherry', 'date']
 
 # Appending an element to a list
 my_list.append('banana')
