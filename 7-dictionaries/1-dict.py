7afba40b877d3efdda20b819dc960c13db458ad6
Hamza Abdullahi <coachbabaa@gmail.com>
2023-03-04 06:20:37 +0100

    Add simple dictionary

---
+++ 7-dictionaries/1-dict.py
@@ -0,0 +1,11 @@
+# Creating a dictionary with key-value pairs
+person = {
+    "name": "John",
+    "age": 30,
+    "gender": "male"
+}
+
+# Accessing values from a dictionary using keys
+print(person["name"])   # Output: John
+print(person["age"])    # Output: 30
+print(person["gender"]) # Output: male
